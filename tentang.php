d4db24938d9782cc569013572cdf0f9ca8cd1ba9
Abid <service@abidhere.my.id>
2024-06-11 06:53:11 +0700

    add link

--- tentang.php
+++ tentang.php
@@ -12,7 +12,7 @@
     <main class="container py-6">
         <div class="w-full rounded-md overflow-hidden shadow-md bg-white py-4 px-4">
             <p class="text-base sm:text-lg md:text-xl">Terimakasih telah mengunjungi website ini!!! <br>
-        Website Tinta Sekolah adalah sebuah website yang memfasilitasi kita untuk mengirimkan sebuah kesan dan pesan pada sebuah acara atau kegiatan dengan tujuan untuk bahan evaluasi ataupun sarana komunikasi antara tamu ataupun pengunjung pada acara atau kegiatan tersebut. Website ini dibuat oleh siswa SMKN 1 Kertajati yang berasal dari jurusan <strong>Rekayasa Perangkat Lunak</strong>.</p>
+        Website Tinta Sekolah adalah sebuah website yang memfasilitasi kita untuk mengirimkan sebuah kesan dan pesan pada sebuah acara atau kegiatan dengan tujuan untuk bahan evaluasi ataupun sarana komunikasi antara tamu ataupun pengunjung pada acara atau kegiatan tersebut. Website ini dibuat dan dikembangkan oleh siswa SMKN 1 Kertajati yang berasal dari jurusan <strong>Rekayasa Perangkat Lunak</strong>. Website ini <a href="https://github.com/AbidAbiyyuImani/tinta-sekolah" class="hover:underline">open source!</a></p>
         </div>
     </main>
 </body>
